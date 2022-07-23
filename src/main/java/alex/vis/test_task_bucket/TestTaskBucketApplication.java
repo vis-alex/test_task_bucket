341ed7b7197684897a21547f94efe4771aaf29fa
vis-alex <usenkoshurik@mail.ru>
2022-07-23 15:49:27 +0300

    Initial commit

---
+++ src/main/java/alex/vis/test_task_bucket/TestTaskBucketApplication.java
@@ -0,0 +1,13 @@
+package alex.vis.test_task_bucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskBucketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskBucketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alex/vis/test_task_bucket/TestTaskBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.vis.test_task_bucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskBucketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
